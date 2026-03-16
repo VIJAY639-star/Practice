f5d69a90fe12f2f6025f488a696a229620cbc198
VIJAY639-star <vijaybolledulavijaybolledula@gmail.com>
2026-03-16 21:48:42 +0530

    Create operators.py with examples of various operators
    Add basic examples of operators in Python.

---
+++ operators.py
@@ -0,0 +1,25 @@
+#opearators in python
+a=5
+b=10
+#arthamatic operators
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#assignment operators
+x=5
+y=15
+z=x*y
+print(z)
+y=z+y
+print(y)
+#logical operators
+print(z>y and y<x)
+#comparision operators
+print(a>=0 or b)
+#membership operators
+flowers=["Rose","Lily","jasmine"]
+print("Red rose" in flowers)
